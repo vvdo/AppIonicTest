7546541e157ed60ba50dc28bde1d61d507ad6290
Vitor Vinicius Duarte Oliveira <vitor.vini@live.com>
2022-08-17 13:30:25 -0300

    Adição dos arquivos do app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./components/pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'button',
+    loadChildren: () => import('./components/pages/button/button.module').then( m => m.ButtonPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
